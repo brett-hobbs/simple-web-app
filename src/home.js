d59b58986adbfd8e4bab20c18e0e5cec5ded870b
Brett Hobbs <brett.hobbs@gmail.com>
2017-02-05 12:08:13 -0800

    Simple app with React/Express/Webpack/Eslint

---
+++ src/home.js
@@ -0,0 +1,9 @@
+const React = require('react');
+
+export default class Home extends React.Component {
+  render() {
+    return <div>Hello World</div>;
+  }
+}
+
+
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: [
+    './index.js'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/assets/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015', 'react']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
