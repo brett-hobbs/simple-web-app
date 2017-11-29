00cb1a6636a01e5b4c95b931d4cf71c87fdadfd1
Brett Hobbs <brett.hobbs@gmail.com>
2017-11-28 19:22:09 -0800

    Add storybook

---
+++ stories/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+import { Home } from '../src/home'
+import { storiesOf } from '@storybook/react'
+
+storiesOf('Sample', module).add('Home', () => <Home />)
--- test/helper.js
+++ test/helper.js
@@ -1,22 +1,22 @@
-'use strict';
-const React = require('react');
-const ReactDOM = require('react-dom');
+'use strict'
+const React = require('react')
+const ReactDOM = require('react-dom')
 
 function renderComponent(desc, props) {
-  const container = global.document.createElement('div');
-  global.document.body.appendChild(container);
+  const container = global.document.createElement('div')
+  global.document.body.appendChild(container)
 
-  const elem = React.createElement(desc, props);
+  const elem = React.createElement(desc, props)
   // eslint-disable-next-line react/no-render-return-value
-  const component = ReactDOM.render(elem, container);
+  const component = ReactDOM.render(elem, container)
 
   if (!component) {
-    throw new Error('renderComponent did not compose child properly');
+    throw new Error('renderComponent did not compose child properly')
   }
 
-  return component;
+  return component
 }
 
 module.exports = {
-  renderComponent,
-};
+  renderComponent
+}
